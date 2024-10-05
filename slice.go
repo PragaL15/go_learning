72c7572e0c46b726e87c3fef061d9c86cbe54b11
Pragalya Kanakaraj <pragalyakanakaraj@gmail.com>
2024-10-05 10:43:02 +0530

    slice is added

--- slice.go
+++ slice.go
@@ -1,1 +1,10 @@
 package main
+
+import "fmt"
+
+func main() {
+    var course = []string{"react", "java", "ruby"}
+    var index int = 2
+    course = append(course[:index], course[index+1:]...)
+    fmt.Println(course)
+}
